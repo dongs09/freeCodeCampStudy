4b5959ca6a3595a019319030a1876507c3c143c2
donna <dongs0909@gmail.com>
2024-03-20 22:33:42 +0900

    calorie counter step 35

--- JavaScriptAndDataStructures/CalorieCounter/script.js
+++ JavaScriptAndDataStructures/CalorieCounter/script.js
@@ -14,9 +14,19 @@ function cleanInputString(str){
     //         cleanStrArray.push(strArray[i]);
     //     }
     // }
-    const regex = /\+-\s/; 
+    const regex = /[+-\s]/g; 
     // +는 정규식에서 특별한 의미가 있는 문자라 \를 앞에 써줘야 함
     // \s는 공백을 의미함
-     
+    // []안에 담으면 +앞에 \가 필요없어짐
+    // []안에 담으면 문자 하나씩 각각 인식
+    // 맨 뒤에 g를 붙이면, global이란 뜻으로, 문장 끝날때까지 매칭된 문자를 계속해서 찾음
+    return str.replace(regex,"");
+}
 
+function isInvalidInput(str){
+    const regex = /\d+e\d+/i;   //숫자만 뽑아내는 정규식
+    // 맨 뒤에 i를 붙으면, insensitive라는 뜻으로, 대소문자 구별없이 인식
+    // +를 붙이면 한번 또는 여러번 매치를 찾음 ex) 1e1도 되고 11e11도 됨
+    // \d는 [0-9]와 같은 뜻이다. digit 이라는 뜻
+    return str.match(regex);
 }
