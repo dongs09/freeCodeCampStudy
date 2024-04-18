9fbd8f9267da41c53cfb3e7729d6dffe6d305a7b
donna <dongs0909@gmail.com>
2024-04-18 18:56:26 +0900

    music player step 34

--- JavaScriptAndDataStructures/MusicPlayer/script.js
+++ JavaScriptAndDataStructures/MusicPlayer/script.js
@@ -115,4 +115,18 @@ const renderSongs = (array) => {
     }).join("");
     playlistSongs.innerHTML = songsHTML;
 };
-renderSongs(userData?.songs);
+
+const sortSongs = () => {
+  userData?.songs.sort((a,b) => {
+    if(a.title < b.title){
+      return -1;
+    }
+    if(a.title > b.title){
+      return 1;
+    }
+    return 0;
+  });
+  return userData?.songs;
+};
+
+renderSongs(sortSongs());
