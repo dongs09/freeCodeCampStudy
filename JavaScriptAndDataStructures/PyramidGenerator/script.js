31517f1bdd80476f965581553ed6565b0e4871d4
donna <dongs0909@gmail.com>
2025-03-28 18:21:28 +0900

    javascript 시작

---
+++ JavaScriptAndDataStructures/PyramidGenerator/script.js
@@ -0,0 +1,35 @@
+//strings are immutable .. string은 불변한다?
+let character = 'Hello';
+//console.log(character);
+//character = "World";
+//console.log(character);
+//let secondCharacter;
+//secondCharacter = character;
+//console.log(secondCharacter);
+
+// let profession = "teacher";
+// let age;
+
+// console.log(profession);
+// console.log(age);
+
+let count = 8;
+//console.log(count + 1);
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+// console.log(rows[0]);
+// rows[rows.length - 1] = 10;
+
+// let cities = ["London", "New York", "Mumbai"];
+// console.log(cities);
+// cities[cities.length-1] = "Mexico City";
+// console.log(cities);
+
+//let pushed = rows.push("freeCodeCamp");
+//console.log(pushed);
+
+//let popped = rows.pop();
+//console.log(popped);
+
+//console.log(rows);
+
