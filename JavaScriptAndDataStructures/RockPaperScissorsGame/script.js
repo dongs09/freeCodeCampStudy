f575ba611f4936f04e391b5dda641b9bee9bb26e
donna <dongs0909@gmail.com>
2025-04-08 13:06:37 +0900

    step4 done

--- JavaScriptAndDataStructures/RockPaperScissorsGame/script.js
+++ JavaScriptAndDataStructures/RockPaperScissorsGame/script.js
@@ -65,5 +65,14 @@ function getRoundResults(userOption) {
      */
 }
 
-console.log(getRoundResults("Rock"));
-console.log("Player Score: ", playerScore, "Computer Score: ", computerScore);
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+
+function showResults(userOption) {
+    roundResultsMsg.innerText = getRoundResults(userOption);
+    playerScoreSpanElement.innerText = playerScore;
+    computerScoreSpanElement.innerText = computerScore;
+};
+
+showResults("Rock");
